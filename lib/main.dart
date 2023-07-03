a6abde20401b76d72530b7e8adc1f37b9858ae3b
ThePerfectMartini <rhd9228@gmail.com>
2023-07-03 18:25:55 +0900

    v7

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("안녕하세요 공성표입니다."),
         ),
+        body: Center(
+          child: Text("공성표 홍성철 전상혁", style: TextStyle(fontSize: 40)),
+        ),
       ),
     );
   }
