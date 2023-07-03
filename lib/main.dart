c6a91ccad6a9e397d5d7a9c996522408655abd5b
ThePerfectMartini <rhd9228@gmail.com>
2023-07-03 19:22:22 +0900

    hotfix: 적용 후 푸시 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-      ),
+          appBar: AppBar(title: Text("핫픽스")),
+          body: Center(
+            child: Text(
+              "공성표 홍성철 전상혁",
+              style: TextStyle(fontSize: 40),
+            ),
+          )),
     );
   }
 }
