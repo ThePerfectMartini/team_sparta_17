b909b5e1cc9393d43f309b86d3f7b7ac2d2b6aae
Hyeok <huk000808@gmail.com>
2023-07-04 18:29:01 +0900

    main 수정 및 edit 상단 바 추가

--- lib/edit.dart
+++ lib/edit.dart
@@ -5,6 +5,31 @@ class Edit extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 207, 255, 229),
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: InkWell(
+                onTap: () {
+                  // 작성한 내용을 날짜화면에 저장하는 작업 및 제목과 내용을 가져와서 처리하는 코드 작성
+                },
+                child: Text(
+                  '작성',
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:team_sparta_17/edit.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,16 +12,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(title: Text("핫픽스")),
-          body: Center(
-            child: Text(
-              "공성표 홍성철 전상혁",
-              style: TextStyle(fontSize: 40),
-            ),
-          )),
+      home: home(),
     );
   }
 }
 
-// hello world : hyeok
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("핫픽스"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "공성표 홍성철 전상혁",
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Edit()),
+                );
+              },
+              child: Text('편집화면으로 이동'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
