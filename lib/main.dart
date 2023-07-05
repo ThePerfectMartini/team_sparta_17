bc9300fb74db6eeb859c01da832575301264d698
godo129 <godo129@naver.com>
2023-07-05 20:17:05 +0900

    fix: dailyScheduleView import 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:team_sparta_17/daily_schduled_view.dart';
+import 'package:team_sparta_17/View/daily_schduled_view.dart';
 import 'HomePage.dart';
 
 void main() {
